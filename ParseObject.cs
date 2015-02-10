1277d55689a19f2e7d7f50fb6c376f4722fe9c96
izrik <izrik@izrik.com>
2015-02-10 14:24:26 -0600

    Override ToString, to provide helpful info while debugging.

--- ParseObject.cs
+++ ParseObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace FbxSharp
 {
@@ -9,6 +10,39 @@ namespace FbxSharp
         public List<object> Values = new List<object>();
         public List<ParseObject> Properties = new List<ParseObject>();
         public bool HasEmptyBlock = true;
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(Name);
+            sb.Append(":");
+            if (Values != null)
+            {
+                sb.Append(" ");
+                if (Values.Count == 0)
+                {
+                }
+                else if (Values.Count == 1)
+                {
+                    sb.Append(Values[0].ToString());
+                }
+                else
+                {
+                    sb.AppendFormat("{0} values", Values.Count);
+                }
+            }
+            if (Properties != null || HasEmptyBlock)
+            {
+                sb.Append(" { ");
+                if (!HasEmptyBlock && Properties.Count > 0)
+                {
+                    sb.AppendFormat("{0} properties", Properties.Count);
+                }
+                sb.Append(" }");
+            }
+
+            return sb.ToString();
+        }
     }
 }
 
