ebd7ef9d87eea137d43ebca8e340ade18fe66edd
izrik <izrik@izrik.com>
2015-02-12 07:38:20 -0600

    ToString and arg checking.

--- Number.cs
+++ Number.cs
@@ -4,28 +4,45 @@ namespace FbxSharp
 {
     public struct Number
     {
-        public Number(string s)
+        public Number(string str)
         {
-            if (string.IsNullOrWhiteSpace(s)) throw new ArgumentNullException("s");
+            if (string.IsNullOrWhiteSpace(str)) throw new ArgumentNullException("s");
 
-            StringRepresentation = s;
+            StringRepresentation = str;
 
             double d;
-            if (double.TryParse(s, out d))
+            if (double.TryParse(str, out d))
                 AsDouble = d;
             else
                 AsDouble = null;
 
             long l;
-            if (long.TryParse(s, out l))
+            if (long.TryParse(str, out l))
                 AsLong = l;
             else
                 AsLong = null;
+
+            if (!AsLong.HasValue && !AsDouble.HasValue)
+            {
+                throw new ArgumentException("The string cannot be interpreted as a snumber", "str");
+            }
         }
 
         public readonly string StringRepresentation;
         public readonly double? AsDouble;
         public readonly long? AsLong;
+
+        public override string ToString()
+        {
+            if (AsLong.HasValue)
+            {
+                return AsLong.Value.ToString();
+            }
+            else
+            {
+                return AsDouble.Value.ToString();
+            }
+        }
     }
 }
 
