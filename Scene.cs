ced645321c21edbb0b44f954a84f7d669ac60223
izrik <izrik@izrik.com>
2015-02-25 09:28:59 -0600

    RootNode

--- Scene.cs
+++ Scene.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FbxSharp
 {
     public class Scene : Document
     {
+        public Scene()
+        {
+            RootNode = new Node();
+            Nodes.Add(RootNode);
+        }
+
+        public List<Node> Nodes = new List<Node>();
+        public Node RootNode { get; protected set; }
     }
 }
 
