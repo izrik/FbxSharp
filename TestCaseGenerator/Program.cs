e6931ab1e08689c7a1010fa746ed309f8032e25d
izrik <izrik@izrik.com>
2015-03-09 12:38:47 -0500

    Add a tool to generate the test cases for us.

---
+++ TestCaseGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCaseGenerator
+{
+    class MainClass
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
