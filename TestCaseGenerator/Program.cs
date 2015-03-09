e328f05d9d658ab84b4162b7c3da4aa3f628d557
izrik <izrik@izrik.com>
2015-03-09 16:22:37 -0500

    Process multiple test case files at once. Include Scene tests.

--- TestCaseGenerator/Program.cs
+++ TestCaseGenerator/Program.cs
@@ -10,146 +10,160 @@ namespace TestCaseGenerator
     {
         public static void Main(string [] args)
         {
-            string type = string.Empty;
+            string language = string.Empty;
 
             if (args.Length > 0)
             {
-                type = args[0];
+                language = args[0];
             }
             else
             {
                 //Console.Write("What output type? [cs]");
                 //type = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(type))
+                if (string.IsNullOrWhiteSpace(language))
                 {
-                    type = "cs";
+                    language = "cs";
                 }
             }
 
-            if (type != "cs" && type != "cpp")
+            if (language != "cs" && language != "cpp")
             {
-                Console.WriteLine("Unrecognized output type: {0}", type);
+                Console.WriteLine("Unrecognized output type: {0}", language);
                 return;
             }
 
-            var fixtures = new List<TestFixture>();
+            string outputType = (args.Length > 1 ? args[1] : "-");
 
-            using (var reader = new StreamReader("test_cases.txt"))
+            var inputs = args.Skip(2).ToList();
+            if (inputs.Count < 1) inputs.Add("test_cases.txt");
+
+            foreach (var input in inputs)
             {
-                TestFixture currentFixture = null;
-                TestCase currentTest = null;
+                var fixtures = new List<TestFixture>();
 
-
-                while (!reader.EndOfStream)
+                using (var reader = new StreamReader(input))
                 {
-                    var line = reader.ReadLine();
-                    if (string.IsNullOrWhiteSpace(line))
-                    {
-                        currentTest.Statements.Add(string.Empty);
-                        continue;
-                    }
+                    TestFixture currentFixture = null;
+                    TestCase currentTest = null;
 
-                    var trimmed = line.Trim();
-                    var parts = trimmed.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-                    string name;
-                    switch (parts[0].ToLower())
-                    {
-                    case "fixture":
-                        name = parts[1];
-                        currentFixture = new TestFixture { Name = name };
-                        fixtures.Add(currentFixture);
-                        break;
-                    case "test":
-                        name = parts[1];
-                        currentTest = new TestCase { Name = name };
-                        currentFixture.TestCases.Add(currentTest);
-                        break;
-                    case "given":
-                    case "require":
-                    case "when":
-                    case "then":
-                        currentTest.Statements.Add(parts[0].ToLower());
-                        break;
-                    default:
-                        currentTest.Statements.Add(trimmed);
-                        break;
-                    }
-                }
-            }
 
-            Console.WriteLine("using System;");
-            Console.WriteLine("using NUnit.Framework;");
-            Console.WriteLine("using FbxSharp;");
-            Console.WriteLine();
-            Console.WriteLine("namespace FbxSharpTests");
-            Console.WriteLine("{");
-            var fixturesStarted = false;
-            foreach (var fixture in fixtures)
-            {
-                if (fixturesStarted) Console.WriteLine();
-                Console.WriteLine("    [TestFixture]");
-                Console.WriteLine("    public class {0}", fixture.Name);
-                Console.WriteLine("    {");
-
-                var casesStarted = false;
-                foreach (var testcase in fixture.TestCases)
-                {
-                    if (casesStarted) Console.WriteLine();
-                    Console.WriteLine("        [Test]");
-                    Console.WriteLine("        public void {0}()", testcase.Name);
-                    Console.WriteLine("        {");
-                    int blanks = 0;
-                    List<String> parts;
-                    foreach (var stmt in testcase.Statements)
+                    while (!reader.EndOfStream)
                     {
-                        if (string.IsNullOrWhiteSpace(stmt))
+                        var line = reader.ReadLine();
+                        if (string.IsNullOrWhiteSpace(line))
                         {
-                            blanks++;
+                            currentTest.Statements.Add(string.Empty);
+                            continue;
                         }
-                        else
+
+                        var trimmed = line.Trim();
+                        var parts = trimmed.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                        string name;
+                        switch (parts[0].ToLower())
                         {
-                            for (; blanks > 0; blanks--) Console.WriteLine();
-                        }
-                        switch (stmt)
-                        {
+                        case "fixture":
+                            name = parts[1];
+                            currentFixture = new TestFixture { Name = name };
+                            fixtures.Add(currentFixture);
+                            break;
+                        case "test":
+                            name = parts[1];
+                            currentTest = new TestCase { Name = name };
+                            currentFixture.TestCases.Add(currentTest);
+                            break;
                         case "given":
                         case "require":
                         case "when":
                         case "then":
-                            Console.WriteLine("            // {0}:", stmt);
+                            currentTest.Statements.Add(parts[0].ToLower());
                             break;
                         default:
-                            var outline = stmt.Replace("AssertEqual", "Assert.AreEqual");
-                            if (Regex.IsMatch(outline, @"\bnew\b"))
-                            {
-                                parts = outline.Split(new[]{ ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
-                                parts[3] = parts[3].Replace("new", "new " + parts[0]);
-                                outline = string.Join(" ", parts);
-                            }
-                            parts = outline.Split(new[]{ ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
-                            if (parts.Count > 3 && parts[2] == "=")
-                            {
-                                parts[0] = "var";
-                                outline = string.Join(" ", parts);
-                            }
-                            if (!string.IsNullOrWhiteSpace(outline))
-                            {
-                                Console.Write("            {0};", outline);
-                                Console.WriteLine();
-                            }
+                            currentTest.Statements.Add(trimmed);
                             break;
                         }
                     }
-
-                    Console.WriteLine("        }");
-                    casesStarted = true;
                 }
 
-                Console.WriteLine("    }");
-                fixturesStarted = true;
-            }
-            Console.WriteLine("}");
+                using (var writer = 
+                    outputType == "-" ? 
+                        Console.Out :
+                        new StreamWriter(input + "." + language))
+                {
+                    writer.WriteLine("using System;");
+                    writer.WriteLine("using NUnit.Framework;");
+                    writer.WriteLine("using FbxSharp;");
+                    writer.WriteLine();
+                    writer.WriteLine("namespace FbxSharpTests");
+                    writer.WriteLine("{");
+                    var fixturesStarted = false;
+                    foreach (var fixture in fixtures)
+                    {
+                        if (fixturesStarted) writer.WriteLine();
+                        writer.WriteLine("    [TestFixture]");
+                        writer.WriteLine("    public class {0}", fixture.Name);
+                        writer.WriteLine("    {");
 
+                        var casesStarted = false;
+                        foreach (var testcase in fixture.TestCases)
+                        {
+                            if (casesStarted) writer.WriteLine();
+                            writer.WriteLine("        [Test]");
+                            writer.WriteLine("        public void {0}()", testcase.Name);
+                            writer.WriteLine("        {");
+                            int blanks = 0;
+                            List<String> parts;
+                            foreach (var stmt in testcase.Statements)
+                            {
+                                if (string.IsNullOrWhiteSpace(stmt))
+                                {
+                                    blanks++;
+                                }
+                                else
+                                {
+                                    for (; blanks > 0; blanks--) writer.WriteLine();
+                                }
+                                switch (stmt)
+                                {
+                                case "given":
+                                case "require":
+                                case "when":
+                                case "then":
+                                    writer.WriteLine("            // {0}:", stmt);
+                                    break;
+                                default:
+                                    var outline = stmt.Replace("AssertEqual", "Assert.AreEqual");
+                                    if (Regex.IsMatch(outline, @"\bnew\b"))
+                                    {
+                                        parts = outline.Split(new[]{ ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                                        parts[3] = parts[3].Replace("new", "new " + parts[0]);
+                                        outline = string.Join(" ", parts);
+                                    }
+                                    parts = outline.Split(new[]{ ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                                    if (parts.Count > 3 && parts[2] == "=")
+                                    {
+                                        parts[0] = "var";
+                                        outline = string.Join(" ", parts);
+                                    }
+                                    if (!string.IsNullOrWhiteSpace(outline))
+                                    {
+                                        writer.Write("            {0};", outline);
+                                        writer.WriteLine();
+                                    }
+                                    break;
+                                }
+                            }
+
+                            writer.WriteLine("        }");
+                            casesStarted = true;
+                        }
+
+                        writer.WriteLine("    }");
+                        fixturesStarted = true;
+                    }
+                    writer.WriteLine("}");
+                    writer.Flush();
+                }
+            }
         }
     }
 }
