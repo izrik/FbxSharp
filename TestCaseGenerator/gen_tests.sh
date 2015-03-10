939d07077d930654ab3501b878e4e140fd0290ec
izrik <izrik@izrik.com>
2015-03-10 11:11:30 -0500

    Don't need that anymore.

--- TestCaseGenerator/gen_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mono bin/Debug/TestCaseGenerator.exe cpp f NodeTest.tc && mv NodeTest.tc.cpp ../../../FbxOpenCpp/NodeTest.cpp
-mono bin/Debug/TestCaseGenerator.exe cs f NodeTest.tc && mv NodeTest.tc.cs ../FbxSharpTests/NodeTest.cs 
-
