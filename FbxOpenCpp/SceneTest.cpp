902528c9fea3a58fd10021bae105debb74e38f2c
izrik <izrik@izrik.com>
2015-03-16 09:22:11 -0500

    Add some test cases for Scene and Pose object connections.

--- FbxOpenCpp/SceneTest.cpp
+++ FbxOpenCpp/SceneTest.cpp
@@ -123,10 +123,101 @@ void Scene_Create_HasRootNode()
     AssertEqual(0, root->GetChildCount());
 }
 
+void Scene_Create_HasZeroPoses()
+{
+    // given:
+    FbxManager* manager = FbxManager::Create();
+
+    // when:
+    FbxScene* scene = FbxScene::Create(manager, "Scene1");
+
+    // then:
+    AssertEqual(0, scene->GetPoseCount());
+}
+
+void Scene_AddPose_AddsPose()
+{
+    // given:
+    FbxManager* manager = FbxManager::Create();
+    FbxScene* scene = FbxScene::Create(manager, "Scene");
+    FbxPose* pose = FbxPose::Create(manager, "Pose");
+
+    // require:
+    AssertEqual(3, scene->GetSrcObjectCount());
+    AssertEqual(scene->GetRootNode(), scene->GetSrcObject(0));
+    AssertEqual(&scene->GetGlobalSettings(), scene->GetSrcObject(1));
+    AssertEqual(scene->GetAnimationEvaluator(), scene->GetSrcObject(2));
+    AssertEqual(0, scene->GetDstObjectCount());
+    AssertEqual(0, scene->GetPoseCount());
+
+    AssertEqual(0, pose->GetSrcObjectCount());
+    AssertEqual(0, pose->GetDstObjectCount());
+    AssertEqual(NULL, pose->GetScene());
+
+    // when:
+    scene->AddPose(pose);
+
+    // then:
+    AssertEqual(4, scene->GetSrcObjectCount());
+    AssertEqual(scene->GetRootNode(), scene->GetSrcObject(0));
+    AssertEqual(&scene->GetGlobalSettings(), scene->GetSrcObject(1));
+    AssertEqual(scene->GetAnimationEvaluator(), scene->GetSrcObject(2));
+    AssertEqual(pose, scene->GetSrcObject(3));
+    AssertEqual(0, scene->GetDstObjectCount());
+    AssertEqual(1, scene->GetPoseCount());
+    AssertEqual(pose, scene->GetPose(0));
+
+    AssertEqual(0, pose->GetSrcObjectCount());
+    AssertEqual(1, pose->GetDstObjectCount());
+    AssertEqual(scene, pose->GetDstObject(0));
+    AssertEqual(scene, pose->GetScene());
+}
+
+void Scene_ConnectSrcObject_AddsPose()
+{
+    // given:
+    FbxManager* manager = FbxManager::Create();
+    FbxScene* scene = FbxScene::Create(manager, "Scene");
+    FbxPose* pose = FbxPose::Create(manager, "Pose");
+
+    // require:
+    AssertEqual(3, scene->GetSrcObjectCount());
+    AssertEqual(scene->GetRootNode(), scene->GetSrcObject(0));
+    AssertEqual(&scene->GetGlobalSettings(), scene->GetSrcObject(1));
+    AssertEqual(scene->GetAnimationEvaluator(), scene->GetSrcObject(2));
+    AssertEqual(0, scene->GetDstObjectCount());
+    AssertEqual(0, scene->GetPoseCount());
+
+    AssertEqual(0, pose->GetSrcObjectCount());
+    AssertEqual(0, pose->GetDstObjectCount());
+    AssertEqual(NULL, pose->GetScene());
+
+    // when:
+    scene->ConnectSrcObject(pose);
+
+    // then:
+    AssertEqual(4, scene->GetSrcObjectCount());
+    AssertEqual(scene->GetRootNode(), scene->GetSrcObject(0));
+    AssertEqual(&scene->GetGlobalSettings(), scene->GetSrcObject(1));
+    AssertEqual(scene->GetAnimationEvaluator(), scene->GetSrcObject(2));
+    AssertEqual(pose, scene->GetSrcObject(3));
+    AssertEqual(0, scene->GetDstObjectCount());
+    AssertEqual(1, scene->GetPoseCount());
+    AssertEqual(pose, scene->GetPose(0));
+
+    AssertEqual(0, pose->GetSrcObjectCount());
+    AssertEqual(1, pose->GetDstObjectCount());
+    AssertEqual(scene, pose->GetDstObject(0));
+    AssertEqual(scene, pose->GetScene());
+}
+
 void SceneTest::RegisterTestCases()
 {
     AddTestCase(Scene_AddNode_AddsNode);
     AddTestCase(RootNode_AddChildNode_AddsNodeToScene);
     AddTestCase(Scene_Create_HasRootNode);
+    AddTestCase(Scene_Create_HasZeroPoses);
+    AddTestCase(Scene_AddPose_AddsPose);
+    AddTestCase(Scene_ConnectSrcObject_AddsPose);
 }
 
