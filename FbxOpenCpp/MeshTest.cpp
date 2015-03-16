b8de67761e5b3bbe67c1b1700ff33901993b5869
izrik <izrik@izrik.com>
2015-03-16 10:23:16 -0500

    Add some tests for control points in GeometryBase, and a test case for creating an FbxMesh object.

--- FbxOpenCpp/MeshTest.cpp
+++ FbxOpenCpp/MeshTest.cpp
@@ -3,7 +3,18 @@
 
 using namespace std;
 
+void Mesh_Create()
+{
+    // given:
+    FbxManager* manager = FbxManager::Create();
+    FbxMesh* mesh = FbxMesh::Create(manager, "Mesh");
+
+    // then:
+    AssertEqual(0, mesh->GetControlPointsCount());
+}
+
 void MeshTest::RegisterTestCases()
 {
+    AddTestCase(Mesh_Create);
 }
 
