9595a4a8b91a89ab06899e30d01c47eb2dff03e0
izrik <izrik@izrik.com>
2015-03-18 10:45:15 -0500

    Fill in materials classes.

--- SurfaceMaterial.cs
+++ SurfaceMaterial.cs
@@ -6,6 +6,19 @@ namespace FbxSharp
 {
     public abstract class SurfaceMaterial : FbxObject
 	{
+        protected SurfaceMaterial(string name="")
+            : base(name)
+        {
+            this.Properties.Add(ShadingModel);
+            this.Properties.Add(MultiLayer);
+        }
+
+        #region Material Properties
+
+        public readonly PropertyT<string> ShadingModel = new PropertyT<string>("ShadingModel");
+        public readonly PropertyT<bool>   MultiLayer   = new PropertyT<bool>("MultiLayer");
+
+        #endregion
 	}
 }
 
--- SurfacePhong.cs
+++ SurfacePhong.cs
@@ -4,6 +4,28 @@ namespace FbxSharp
 {
     public class SurfacePhong : SurfaceLambert
     {
+        public SurfacePhong(string name="")
+            : base(name)
+        {
+            this.Properties.AddRange(
+                new Property[] {
+                    Specular,
+                    SpecularFactor,
+                    Shininess,
+                    Reflection,
+                    ReflectionFactor,
+                });
+        }
+
+        #region Material properties
+
+        public readonly PropertyT<Vector3>  Specular            = new PropertyT<Vector3>("Specular");
+        public readonly PropertyT<double>   SpecularFactor      = new PropertyT<double>("SpecularFactor");
+        public readonly PropertyT<double>   Shininess           = new PropertyT<double>("Shininess");
+        public readonly PropertyT<Vector3>  Reflection          = new PropertyT<Vector3>("Reflection");
+        public readonly PropertyT<double>   ReflectionFactor    = new PropertyT<double>("ReflectionFactor");
+
+        #endregion
     }
 }
 
