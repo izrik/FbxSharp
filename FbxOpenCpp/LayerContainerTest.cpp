beb34ed107b5dae9b891f4429d09dbab979e1379
izrik <izrik@izrik.com>
2015-03-18 07:09:13 -0500

    Add a test for the creation of layer containers.

--- FbxOpenCpp/LayerContainerTest.cpp
+++ FbxOpenCpp/LayerContainerTest.cpp
@@ -3,7 +3,22 @@
 
 using namespace std;
 
+void LayerContainer_Create()
+{
+    // given:
+    FbxManager* manager = FbxManager::Create();
+
+    // when:
+    FbxLayerContainer* lc = FbxLayerContainer::Create(manager, "");
+
+    // then:
+    AssertEqual(0, lc->GetSrcObjectCount());
+    AssertEqual(0, lc->GetDstObjectCount());
+    AssertEqual(0, lc->GetLayerCount());
+}
+
 void LayerContainerTest::RegisterTestCases()
 {
+    AddTestCase(LayerContainer_Create);
 }
 
