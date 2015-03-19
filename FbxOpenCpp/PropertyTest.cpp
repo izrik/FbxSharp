2d3433765886d9308ee7634620d8d725cfe7ed68
izrik <izrik@izrik.com>
2015-03-19 11:03:37 -0500

    Test searching for a property by name.

--- FbxOpenCpp/PropertyTest.cpp
+++ FbxOpenCpp/PropertyTest.cpp
@@ -3,7 +3,21 @@
 
 using namespace std;
 
+void SurfacePhong_FindProperty_FindsProperty()
+{
+    // given:
+    FbxManager* manager = FbxManager::Create();
+    FbxSurfacePhong* surf = FbxSurfacePhong::Create(manager, "");
+
+    // when:
+    FbxProperty prop = surf->FindProperty("SpecularColor");
+
+    // then:
+    AssertTrue(prop.IsValid());
+}
+
 void PropertyTest::RegisterTestCases()
 {
+    AddTestCase(SurfacePhong_FindProperty_FindsProperty);
 }
 
