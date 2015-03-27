3ffb4a64f85e0213d31002c22b0709bae2f71e58
izrik <izrik@izrik.com>
2015-03-27 08:30:06 -0500

    Create method stubs for connections.

--- Property.cs
+++ Property.cs
@@ -13,6 +13,7 @@ namespace FbxSharp
         #region Property Identification
 
         public string Name { get; protected set; }
+
         public abstract Type PropertyDataType { get; }
         //public Object FbxObject { get; protected set; }
 
@@ -28,7 +29,7 @@ namespace FbxSharp
             throw new NotImplementedException();
         }
 
-        public string GetLabel(bool pReturnNameIfEmpty=true)
+        public string GetLabel(bool pReturnNameIfEmpty = true)
         {
             throw new NotImplementedException();
         }
@@ -56,20 +57,26 @@ namespace FbxSharp
         {
             throw new NotImplementedException();
         }
+
         public virtual object GetValue()
         {
             throw new NotImplementedException();
         }
+
         public virtual bool Set<T>(T pValue)
         {
             throw new NotImplementedException();
         }
+
         public virtual bool Set(object value)
         {
             throw new NotImplementedException();
         }
 
-        public virtual bool IsValid() { return true; }
+        public virtual bool IsValid()
+        {
+            return true;
+        }
 
         public PropertyFlags.EInheritType GetValueInheritType()
         {
@@ -94,13 +101,284 @@ namespace FbxSharp
             return string.Format("{0}: {1}", Name, GetValue());
         }
 
-        public List<FbxObject> SrcObjects = new List<FbxObject>();
+        #region General Object Connection and Relationship Management
 
-        public bool ConnectSrcObject(FbxObject src, Connection.EType type=Connection.EType.None)
+        public bool ConnectSrcObject(FbxObject pObject, Connection.EType pType=Connection.EType.None)
         {
-            SrcObjects.Add(src);
-            return true;
+            throw new NotImplementedException();
         }
+
+        public bool IsConnectedSrcObject(FbxObject pObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DisconnectSrcObject(FbxObject pObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DisconnectAllSrcObject()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public bool DisconnectAllSrcObject(FbxCriteria pCriteria)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public int GetSrcObjectCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public int GetSrcObjectCount(FbxCriteria pCriteria)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public FbxObject GetSrcObject(int pIndex=0)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public FbxObject GetSrcObject(FbxCriteria pCriteria, int pIndex=0)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public FbxObject FindSrcObject(string pName, int pStartIndex=0)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public FbxObject FindSrcObject(FbxCriteria pCriteria, string pName, int pStartIndex=0)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public bool DisconnectAllSrcObject<T>()
+            where T : FbxObject
+        {
+            throw new NotImplementedException();
+        }
+
+        //public bool DisconnectAllSrcObject<T>(FbxCriteria pCriteria)
+        //    where T : FbxObject
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public int GetSrcObjectCount<T>()
+            where T : FbxObject
+        {
+            throw new NotImplementedException();
+        }
+
+        //public int GetSrcObjectCount<T>(FbxCriteria pCriteria)
+        //    where T : FbxObject
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public T GetSrcObject<T>(int pIndex=0)
+            where T : FbxObject
+        {
+            throw new NotImplementedException();
+        }
+
+        //public T GetSrcObject<T>(FbxCriteria pCriteria, int pIndex=0)
+        //    where T : FbxObject
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public T FindSrcObject<T>(string pName, int pStartIndex=0)
+            where T : FbxObject
+        {
+            throw new NotImplementedException();
+        }
+
+        //public T FindSrcObject<T>(FbxCriteria pCriteria, string pName, int pStartIndex=0)
+        //    where T : FbxObject
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public bool ConnectDstObject(FbxObject pObject, Connection.EType pType=Connection.EType.None)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConnectedDstObject(FbxObject pObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DisconnectDstObject(FbxObject pObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DisconnectAllDstObject()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public bool DisconnectAllDstObject(FbxCriteria pCriteria)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public int GetDstObjectCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public int GetDstObjectCount(FbxCriteria pCriteria)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public FbxObject GetDstObject(int pIndex=0)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public FbxObject GetDstObject(FbxCriteria pCriteria, int pIndex=0)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public FbxObject FindDstObject(string pName, int pStartIndex=0)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public FbxObject FindDstObject(FbxCriteria pCriteria, string pName, int pStartIndex=0)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public bool DisconnectAllDstObject<T>()
+            where T : FbxObject
+        {
+            throw new NotImplementedException();
+        }
+
+        //public bool DisconnectAllDstObject<T>(FbxCriteria pCriteria)
+        //    where T : FbxObject
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public int GetDstObjectCount<T>()
+            where T : FbxObject
+        {
+            throw new NotImplementedException();
+        }
+
+        //public int GetDstObjectCount<T>(FbxCriteria pCriteria)
+        //    where T : FbxObject
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public T GetDstObject<T>(int pIndex=0)
+            where T : FbxObject
+        {
+            throw new NotImplementedException();
+        }
+
+        //public T GetDstObject<T>(FbxCriteria pCriteria, int pIndex=0)
+        //    where T : FbxObject
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public T FindDstObject<T>(string pName, int pStartIndex=0)
+            where T : FbxObject
+        {
+            throw new NotImplementedException();
+        }
+
+        //public T FindDstObject<T>(FbxCriteria pCriteria, string pName, int pStartIndex=0)
+        //    where T : FbxObject
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        #endregion
+
+        #region General Property Connection and Relationship Management
+
+        public bool ConnectSrcProperty(Property pProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConnectedSrcProperty(Property pProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DisconnectSrcProperty(Property pProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetSrcPropertyCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ConnectDstProperty(Property pProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConnectedDstProperty(Property pProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DisconnectDstProperty(Property pProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDstPropertyCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearConnectCache()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Property GetSrcProperty(int pIndex=0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Property FindSrcProperty(string pName, int pStartIndex=0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Property GetDstProperty(int pIndex=0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Property FindDstProperty(string pName, int pStartIndex=0)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
 
