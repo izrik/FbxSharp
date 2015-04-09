afee57fe8cd1c2fffd1d20661049b2c51e2ca594
izrik <izrik@izrik.com>
2015-04-09 10:07:01 -0500

    public

--- AnimEvaluator.cs
+++ AnimEvaluator.cs
@@ -4,91 +4,91 @@ namespace FbxSharp
 {
     public class AnimEvaluator : FbxObject
     {
-        Matrix GetNodeGlobalTransform(Node pNode)
+        public Matrix GetNodeGlobalTransform(Node pNode)
         {
             return GetNodeGlobalTransform(pNode, FbxTime.Infinite);
         }
-        Matrix GetNodeGlobalTransform(Node pNode, FbxTime pTime, Node.EPivotSet pPivotSet=Node.EPivotSet.eSourcePivot, bool pApplyTarget=false, bool pForceEval=false)
+        public Matrix GetNodeGlobalTransform(Node pNode, FbxTime pTime, Node.EPivotSet pPivotSet=Node.EPivotSet.eSourcePivot, bool pApplyTarget=false, bool pForceEval=false)
         {
             throw new NotImplementedException();
         }
 
-        Matrix GetNodeLocalTransform(Node pNode)
+        public Matrix GetNodeLocalTransform(Node pNode)
         {
             return GetNodeLocalTransform(pNode, FbxTime.Infinite);
         }
-        Matrix GetNodeLocalTransform(Node pNode, FbxTime pTime, Node.EPivotSet pPivotSet=Node.EPivotSet.eSourcePivot, bool pApplyTarget=false, bool pForceEval=false)
+        public Matrix GetNodeLocalTransform(Node pNode, FbxTime pTime, Node.EPivotSet pPivotSet=Node.EPivotSet.eSourcePivot, bool pApplyTarget=false, bool pForceEval=false)
         {
             throw new NotImplementedException();
         }
 
-        Vector4 GetNodeLocalTranslation(Node pNode)
+        public Vector4 GetNodeLocalTranslation(Node pNode)
         {
             return GetNodeLocalTranslation(pNode, FbxTime.Infinite);
         }
-        Vector4 GetNodeLocalTranslation(Node pNode, FbxTime pTime, Node.EPivotSet pPivotSet=Node.EPivotSet.eSourcePivot, bool pApplyTarget=false, bool pForceEval=false)
+        public Vector4 GetNodeLocalTranslation(Node pNode, FbxTime pTime, Node.EPivotSet pPivotSet=Node.EPivotSet.eSourcePivot, bool pApplyTarget=false, bool pForceEval=false)
         {
             throw new NotImplementedException();
         }
 
-        Vector4 GetNodeLocalRotation(Node pNode)
+        public Vector4 GetNodeLocalRotation(Node pNode)
         {
             return GetNodeLocalRotation(pNode, FbxTime.Infinite);
         }
-        Vector4 GetNodeLocalRotation(Node pNode, FbxTime pTime, Node.EPivotSet pPivotSet=Node.EPivotSet.eSourcePivot, bool pApplyTarget=false, bool pForceEval=false)
+        public Vector4 GetNodeLocalRotation(Node pNode, FbxTime pTime, Node.EPivotSet pPivotSet=Node.EPivotSet.eSourcePivot, bool pApplyTarget=false, bool pForceEval=false)
         {
             throw new NotImplementedException();
         }
 
-        Vector4 GetNodeLocalScaling(Node pNode)
+        public Vector4 GetNodeLocalScaling(Node pNode)
         {
             return GetNodeLocalScaling(pNode, FbxTime.Infinite);
         }
-        Vector4 GetNodeLocalScaling(Node pNode, FbxTime pTime, Node.EPivotSet pPivotSet=Node.EPivotSet.eSourcePivot, bool pApplyTarget=false, bool pForceEval=false)
+        public Vector4 GetNodeLocalScaling(Node pNode, FbxTime pTime, Node.EPivotSet pPivotSet=Node.EPivotSet.eSourcePivot, bool pApplyTarget=false, bool pForceEval=false)
         {
             throw new NotImplementedException();
         }
 
-        T GetPropertyValue<T>(Property pProperty, FbxTime pTime, bool pForceEval=false)
+        public T GetPropertyValue<T>(Property pProperty, FbxTime pTime, bool pForceEval=false)
         {
             throw new NotImplementedException();
         }
 
-        //FbxPropertyValue GetPropertyValue(Property pProperty, FbxTime pTime, bool pForceEval=false)
+        //public FbxPropertyValue GetPropertyValue(Property pProperty, FbxTime pTime, bool pForceEval=false)
         //{
         //    throw new NotImplementedException();
         //}
 
-        AnimationCurveNode GetPropertyCurveNode(Property pProperty, AnimationLayer pAnimLayer)
+        public AnimationCurveNode GetPropertyCurveNode(Property pProperty, AnimationLayer pAnimLayer)
         {
             throw new NotImplementedException();
         }
 
-        FbxTime ValidateTime(FbxTime pTime)
+        public FbxTime ValidateTime(FbxTime pTime)
         {
             throw new NotImplementedException();
         }
 
-        void Reset()
+        public void Reset()
         {
             throw new NotImplementedException();
         }
 
-        void Flush(Node pNode)
+        public void Flush(Node pNode)
         {
             throw new NotImplementedException();
         }
 
-        void Flush(Property pProperty)
+        public void Flush(Property pProperty)
         {
             throw new NotImplementedException();
         }
 
-        void ComputeLocalTRSFromGlobal(out Vector4 pRetLT, out Vector4 pRetLR, out Vector4 pRetLS, Node pNode, Matrix pGX)
+        public void ComputeLocalTRSFromGlobal(out Vector4 pRetLT, out Vector4 pRetLR, out Vector4 pRetLS, Node pNode, Matrix pGX)
         {
             ComputeLocalTRSFromGlobal(out pRetLT, out pRetLR, out pRetLS, pNode, pGX, FbxTime.Infinite);
         }
-        void ComputeLocalTRSFromGlobal(out Vector4 pRetLT, out Vector4 pRetLR, out Vector4 pRetLS, Node pNode, Matrix pGX, FbxTime pTime, Node.EPivotSet pPivotSet=Node.EPivotSet.eSourcePivot, bool pApplyTarget=false, bool pForceEval=false)
+        public void ComputeLocalTRSFromGlobal(out Vector4 pRetLT, out Vector4 pRetLR, out Vector4 pRetLS, Node pNode, Matrix pGX, FbxTime pTime, Node.EPivotSet pPivotSet=Node.EPivotSet.eSourcePivot, bool pApplyTarget=false, bool pForceEval=false)
         {
             throw new NotImplementedException();
         }
