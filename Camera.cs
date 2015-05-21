83223ff18f2967fa546a31d59782767582b84d1f
izrik <izrik@izrik.com>
2015-05-21 06:59:46 -0500

    Add the empty Camera class.

---
+++ Camera.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FbxSharp
+{
+    public class Camera : NodeAttribute
+    {
+        public Camera(string name="")
+            : base(name)
+        {
+        }
+
+        #region implemented abstract members of NodeAttribute
+
+        public override EAttributeType AttributeType {
+            get {
+                return EAttributeType.Camera;
+            }
+        }
+
+        #endregion
+    }
+}
+
