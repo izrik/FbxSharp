f1deaf7c927e7e6bb713e5c47d16a56dd14fc468
izrik <izrik@izrik.com>
2015-10-14 17:16:16 -0500

    Create and upload the Nuget package after a successful tagged build.

---
+++ nuget-pack-push.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+
+VERSION=`echo "$1" | perl -ne 'print /^v(\d+\.\d+(?:\.\d+(?:\.\d+)?)?)$/'`
+if [ "$VERSION" = "" ];
+then
+    echo 'Wrong version format. Package will not be created.'
+    exit
+fi
+
+nuget pack FbxSharp.nuspec -Properties version=$VERSION && \
+    nuget push -ApiKey $NUGET_APIKEY FbxSharp.$VERSION.nupkg
