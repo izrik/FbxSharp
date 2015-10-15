6990297ecd0583486cedb1c1f262f4f6414134fa
izrik <izrik@izrik.com>
2015-10-15 13:40:58 -0500

    Check the actual assembly version. Don't deploy if it doesn't match the tag version.

--- nuget-pack-push.sh
+++ nuget-pack-push.sh
@@ -4,7 +4,15 @@ VERSION=`echo "$1" | perl -ne 'print /^v(\d+\.\d+(?:\.\d+(?:\.\d+)?)?)$/'`
 if [ "$VERSION" = "" ];
 then
     echo 'Wrong version format. Package will not be created.'
-    exit
+    exit 1
+fi
+
+AVERSION=`grep AssemblyVersion AssemblyInfo.cs | perl -npe 's/^.*?\"//;s/\".*$//'`
+
+if [ "$VERSION" != "$AVERSION ]
+then
+    echo "Tag doesn't match assembly version. Package will not be created."
+    exit 1
 fi
 
 nuget pack FbxSharp.nuspec -Properties version=$VERSION && \
