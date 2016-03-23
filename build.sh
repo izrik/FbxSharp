cc75737f7a47eb6b1199eddf471e100a6c78320f
izrik <izrik@izrik.com>
2016-03-23 10:09:39 -0500

    Use 'set -e' instead of cascading ampersands.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-
-nuget restore FbxSharpBuild.sln && \
-  xbuild /p:Configuration=Debug FbxSharpBuild.sln && \
-  nunit-console ./FbxSharpTests/bin/Debug/FbxSharpTests.dll
+#!/bin/bash
+set -e
+nuget restore FbxSharpBuild.sln
+xbuild /p:Configuration=Debug FbxSharpBuild.sln
+nunit-console ./FbxSharpTests/bin/Debug/FbxSharpTests.dll
