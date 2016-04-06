3267b13550b00748efb51e2451656d583e7d752f
izrik <izrik@izrik.com>
2016-04-06 15:12:25 -0500

    Add a test class.

---
+++ FbxSharpTests/ObjectPrinterTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using FbxSharp;
+
+namespace FbxSharpTests
+{
+    [TestFixture]
+    public class ObjectPrinterTest
+    {
+        [Test]
+        public void QuoteQuotesAndEscapeStrings()
+        {
+            Assert.AreEqual("\"abcdefghijklmnopqrstuvwxyz\"", ObjectPrinter.quote("abcdefghijklmnopqrstuvwxyz"));
+            Assert.AreEqual("\"ABCDEFGHIJKLMNOPQRSTUVWXYZ\"", ObjectPrinter.quote("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+            Assert.AreEqual("\"0123456789\"", ObjectPrinter.quote("0123456789"));
+            Assert.AreEqual("\"`~!@#$%^&*()_+-=\"", ObjectPrinter.quote("`~!@#$%^&*()_+-="));
+//            Assert.AreEqual("\"\\\\\"", ObjectPrinter.quote("\\"));
+//            Assert.AreEqual("\"\\\"\"", ObjectPrinter.quote("\""));
+            Assert.AreEqual("\"[]{}|;:',<.>/?\"", ObjectPrinter.quote("[]{}|;:',<.>/?"));
+            Assert.AreEqual("\"\\r\"", ObjectPrinter.quote("\r"));
+            Assert.AreEqual("\"\\n\"", ObjectPrinter.quote("\n"));
+            Assert.AreEqual("\"\\t\"", ObjectPrinter.quote("\t"));
+        }
+    }
+}
+
