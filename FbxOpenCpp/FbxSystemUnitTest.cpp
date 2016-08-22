f16854a75cd8876e7e87f971d80b5c896fd99dab
izrik <izrik@izrik.com>
2016-08-22 11:37:47 -0500

    Add more tests for the constructors.

--- FbxOpenCpp/FbxSystemUnitTest.cpp
+++ FbxOpenCpp/FbxSystemUnitTest.cpp
@@ -29,9 +29,104 @@ void FbxSystemUnitTest_Create_DefaultMultiplierIsOne()
     AssertEqual(1, su->GetMultiplier(), 0.00001);
 }
 
+void FbxSystemUnitTest_Create_SettingScaleFactorSetsScaleFactor()
+{
+    // given:
+    FbxManager* manager = FbxManager::Create();
+    FbxSystemUnit* su;
+
+    // when:
+    su = new FbxSystemUnit(2);
+
+    // then:
+    AssertEqual(2, su->GetScaleFactor(), 0.00001);
+    AssertEqual(1, su->GetMultiplier(), 0.00001);
+
+    // when:
+    su = new FbxSystemUnit(2.3);
+
+    // then:
+    AssertEqual(2.3, su->GetScaleFactor(), 0.00001);
+    AssertEqual(1, su->GetMultiplier(), 0.00001);
+}
+
+void FbxSystemUnitTest_Create_SettingMultiplierSetsMultiplier()
+{
+    // given:
+    FbxManager* manager = FbxManager::Create();
+    FbxSystemUnit* su;
+
+    // when:
+    su = new FbxSystemUnit(1, 3);
+
+    // then:
+    AssertEqual(1, su->GetScaleFactor(), 0.00001);
+    AssertEqual(3, su->GetMultiplier(), 0.00001);
+
+    // when:
+    su = new FbxSystemUnit(1, 4.5);
+
+    // then:
+    AssertEqual(1, su->GetScaleFactor(), 0.00001);
+    AssertEqual(4.5, su->GetMultiplier(), 0.00001);
+}
+
+void FbxSystemUnitTest_Create_SettingBothSetsBoth()
+{
+    // given:
+    FbxManager* manager = FbxManager::Create();
+    FbxSystemUnit* su;
+
+    // when:
+    su = new FbxSystemUnit(5, 6);
+
+    // then:
+    AssertEqual(5, su->GetScaleFactor(), 0.00001);
+    AssertEqual(6, su->GetMultiplier(), 0.00001);
+
+    // when:
+    su = new FbxSystemUnit(7.8, 8.9);
+
+    // then:
+    AssertEqual(7.8, su->GetScaleFactor(), 0.00001);
+    AssertEqual(8.9, su->GetMultiplier(), 0.00001);
+}
+
+void FbxSystemUnitTest_Create_NegativeValuesAreAllowed()
+{
+    // given:
+    FbxManager* manager = FbxManager::Create();
+    FbxSystemUnit* su;
+
+    // when:
+    su = new FbxSystemUnit(-2, 1);
+
+    // then:
+    AssertEqual(-2, su->GetScaleFactor(), 0.00001);
+    AssertEqual(1, su->GetMultiplier(), 0.00001);
+
+    // when:
+    su = new FbxSystemUnit(1, -3);
+
+    // then:
+    AssertEqual(1, su->GetScaleFactor(), 0.00001);
+    AssertEqual(-3, su->GetMultiplier(), 0.00001);
+
+    // when:
+    su = new FbxSystemUnit(-4, -5);
+
+    // then:
+    AssertEqual(-4, su->GetScaleFactor(), 0.00001);
+    AssertEqual(-5, su->GetMultiplier(), 0.00001);
+}
+
 void FbxSystemUnitTest::RegisterTestCases()
 {
     AddTestCase(FbxSystemUnitTest_Create_DefaultScaleFactorIsOne);
     AddTestCase(FbxSystemUnitTest_Create_DefaultMultiplierIsOne);
+    AddTestCase(FbxSystemUnitTest_Create_SettingScaleFactorSetsScaleFactor);
+    AddTestCase(FbxSystemUnitTest_Create_SettingMultiplierSetsMultiplier);
+    AddTestCase(FbxSystemUnitTest_Create_SettingBothSetsBoth);
+    AddTestCase(FbxSystemUnitTest_Create_NegativeValuesAreAllowed);
 }
 
--- FbxSharpTests/FbxSystemUnitTest.cs
+++ FbxSharpTests/FbxSystemUnitTest.cs
@@ -32,5 +32,96 @@ namespace FbxSharpTests
             // then:
             Assert.AreEqual(1, su.GetMultiplier(), 0.00001);
         }
+
+        [Test]
+        public void FbxSystemUnitTest_Create_SettingScaleFactorSetsScaleFactor()
+        {
+            // given:
+            FbxSystemUnit su;
+
+            // when:
+            su = new FbxSystemUnit(2);
+
+            // then:
+            Assert.AreEqual(2, su.GetScaleFactor(), 0.00001);
+            Assert.AreEqual(1, su.GetMultiplier(), 0.00001);
+
+            // when:
+            su = new FbxSystemUnit(2.3);
+
+            // then:
+            Assert.AreEqual(2.3, su.GetScaleFactor(), 0.00001);
+            Assert.AreEqual(1, su.GetMultiplier(), 0.00001);
+        }
+
+        [Test]
+        public void FbxSystemUnitTest_Create_SettingMultiplierSetsMultiplier()
+        {
+            // given:
+            FbxSystemUnit su;
+
+            // when:
+            su = new FbxSystemUnit(1, 3);
+
+            // then:
+            Assert.AreEqual(1, su.GetScaleFactor(), 0.00001);
+            Assert.AreEqual(3, su.GetMultiplier(), 0.00001);
+
+            // when:
+            su = new FbxSystemUnit(1, 4.5);
+
+            // then:
+            Assert.AreEqual(1, su.GetScaleFactor(), 0.00001);
+            Assert.AreEqual(4.5, su.GetMultiplier(), 0.00001);
+        }
+
+        [Test]
+        public void FbxSystemUnitTest_Create_SettingBothSetsBoth()
+        {
+            // given:
+            FbxSystemUnit su;
+
+            // when:
+            su = new FbxSystemUnit(5, 6);
+
+            // then:
+            Assert.AreEqual(5, su.GetScaleFactor(), 0.00001);
+            Assert.AreEqual(6, su.GetMultiplier(), 0.00001);
+
+            // when:
+            su = new FbxSystemUnit(7.8, 8.9);
+
+            // then:
+            Assert.AreEqual(7.8, su.GetScaleFactor(), 0.00001);
+            Assert.AreEqual(8.9, su.GetMultiplier(), 0.00001);
+        }
+
+        [Test]
+        public void FbxSystemUnitTest_Create_NegativeValuesAreAllowed()
+        {
+            // given:
+            FbxSystemUnit su;
+
+            // when:
+            su = new FbxSystemUnit(-2, 1);
+
+            // then:
+            Assert.AreEqual(-2, su.GetScaleFactor(), 0.00001);
+            Assert.AreEqual(1, su.GetMultiplier(), 0.00001);
+
+            // when:
+            su = new FbxSystemUnit(1, -3);
+
+            // then:
+            Assert.AreEqual(1, su.GetScaleFactor(), 0.00001);
+            Assert.AreEqual(-3, su.GetMultiplier(), 0.00001);
+
+            // when:
+            su = new FbxSystemUnit(-4, -5);
+
+            // then:
+            Assert.AreEqual(-4, su.GetScaleFactor(), 0.00001);
+            Assert.AreEqual(-5, su.GetMultiplier(), 0.00001);
+        }
     }
 }
