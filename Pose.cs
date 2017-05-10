bfae412b5a653924a54248909f2a43abd2066ad6
izrik <izrik@izrik.com>
2017-05-09 23:46:48 -0500

    Move PoseInfo to its own file.

--- Pose.cs
+++ Pose.cs
@@ -16,20 +16,6 @@ namespace FbxSharp
         readonly ChangeNotifyList<PoseInfo> poseInfos = new ChangeNotifyList<PoseInfo>();
         public readonly CollectionView<PoseInfo, PoseInfo> PoseInfos;
 
-        public struct PoseInfo
-        {
-            public PoseInfo(FbxNode node, FbxMatrix matrix, bool matrixIsLocal=false)
-            {
-                Node = node;
-                Matrix = matrix;
-                MatrixIsLocal = matrixIsLocal;
-            }
-
-            public readonly FbxNode Node;
-            public readonly FbxMatrix Matrix;
-            public readonly bool MatrixIsLocal;
-        }
-
         public void SetIsBindPose(bool pIsBindPose)
         {
             isBindPose = pIsBindPose;
