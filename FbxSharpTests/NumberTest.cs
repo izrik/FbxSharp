60ff1008d2d7517608fbb70279456e7715299054
izrik <izrik@izrik.com>
2018-12-20 21:22:36 +0000

    Add tests for different kinds of numbers.

--- FbxSharpTests/NumberTest.cs
+++ FbxSharpTests/NumberTest.cs
@@ -19,5 +19,66 @@ namespace FbxSharpTests
 	    Assert.IsNull(n.StringRepresentation);
 	    // Assert.AreEqual("null", n.ToString());
         }
+
+	[Test]
+	public void CreateWithLong_HasLongAndDoubleValue()
+	{
+	    // when
+	    var n = new Number("123");
+
+	    // then
+	    Assert.IsTrue(n.AsDouble.HasValue);
+	    Assert.AreEqual(123d, n.AsDouble.Value);
+	    Assert.IsTrue(n.AsLong.HasValue);
+	    Assert.AreEqual(123, n.AsLong.Value);
+	    Assert.AreEqual("123", n.ToString());
+	    Assert.AreEqual("123", n.StringRepresentation);
+
+	    // when
+	    n = new Number("12345678900");
+
+	    // then
+	    Assert.IsTrue(n.AsDouble.HasValue);
+	    Assert.AreEqual(12345678900d, n.AsDouble.Value);
+	    Assert.IsTrue(n.AsLong.HasValue);
+	    Assert.AreEqual(12345678900, n.AsLong.Value);
+	    Assert.AreEqual("12345678900", n.ToString());
+	    Assert.AreEqual("12345678900", n.StringRepresentation);
+
+	    // when
+	    n = new Number("0");
+
+	    // then
+	    Assert.IsTrue(n.AsDouble.HasValue);
+	    Assert.AreEqual(0d, n.AsDouble.Value);
+	    Assert.IsTrue(n.AsLong.HasValue);
+	    Assert.AreEqual(0L, n.AsLong.Value);
+	    Assert.AreEqual("0", n.ToString());
+	    Assert.AreEqual("0", n.StringRepresentation);
+	}
+
+	[Test]
+	public void CreateWithDouble_HasOnlyDoubleValue()
+	{
+	    // when
+	    var n = new Number("0.0");
+
+	    // then
+	    Assert.IsTrue(n.AsDouble.HasValue);
+	    Assert.AreEqual(0d, n.AsDouble.Value);
+	    Assert.IsFalse(n.AsLong.HasValue);
+	    Assert.AreEqual("0", n.ToString());
+	    Assert.AreEqual("0.0", n.StringRepresentation);
+
+	    // when
+	    n = new Number("0.000000000000000");
+
+	    // then
+	    Assert.IsTrue(n.AsDouble.HasValue);
+	    Assert.AreEqual(0d, n.AsDouble.Value);
+	    Assert.IsFalse(n.AsLong.HasValue);
+	    Assert.AreEqual("0", n.ToString());
+	    Assert.AreEqual("0.000000000000000", n.StringRepresentation);
+	}
     }
 }
