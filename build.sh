d04379e761b1484c1334aca4205fcb3491af5cb9
izrik <izrik@izrik.com>
2019-03-26 15:24:01 -0500

    Activate the coverage profiler.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 csc HelloWorld.cs
-mono HelloWorld.exe
+mono --profile=coverage HelloWorld.exe
 #nuget restore FbxSharpBuild.sln
 #msbuild /p:Configuration=Debug FbxSharpBuild.sln
 #nunit-console ./FbxSharpTests/bin/Debug/FbxSharpTests.dll
