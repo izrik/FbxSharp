04670339be34ee23392716dbc0f23ad04851712c
izrik <izrik@izrik.com>
2019-03-26 15:54:28 -0500

    Add a custom exception class for the parser.

---
+++ ParseException.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class ParseException : Exception
+{
+    public ParseException() 
+        : base()
+    {
+    }
+
+    public ParseException(string message)
+        : base(message)
+    {
+    }
+
+    public ParseException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
