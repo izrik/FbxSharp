82a7900764df3d19d9861ef826061783e587c062
izrik <izrik@izrik.com>
2019-03-26 15:41:08 -0500

    Add a custom exception class for the tokenizer.

---
+++ TokenizationException.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class TokenizationException : Exception
+{
+    public TokenizationException() 
+        : base()
+    {
+    }
+
+    public TokenizationException(string message)
+        : base(message)
+    {
+    }
+
+    public TokenizationException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
