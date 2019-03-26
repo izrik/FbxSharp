7dac8cca885356785d850efe98d10a15a757929b
izrik <izrik@izrik.com>
2019-03-26 10:08:53 -0500

    Update build tool names.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -e
 nuget restore FbxSharpBuild.sln
 msbuild /p:Configuration=Debug FbxSharpBuild.sln
-nunit-console-2.6 ./FbxSharpTests/bin/Debug/FbxSharpTests.dll
+nunit-console ./FbxSharpTests/bin/Debug/FbxSharpTests.dll
