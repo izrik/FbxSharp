ba1cbec9a997aced648b57867ebbbf8786be6311
izrik <izrik@izrik.com>
2019-03-26 22:33:09 -0500

    Include the location in the exception messages.

--- ParseException.cs
+++ ParseException.cs
@@ -4,9 +4,19 @@ namespace FbxSharp
 {
     public class ParseException : Exception
     {
-        public ParseException()
-            : base()
+        public ParseException(InputLocation location, string message)
+            : this(FormatMessage(location, message))
         {
+        }
+
+        public ParseException(InputLocation location, string message, Exception inner)
+            : this(FormatMessage(location, message), inner)
+        {
+        }
+
+	static string FormatMessage(InputLocation location, string message)
+	{
+            return string.Format("at {0}:{1},{2}: {3}", location.Filename, location.Line, location.Column, message);
         }
 
         public ParseException(string message)
--- TokenizationException.cs
+++ TokenizationException.cs
@@ -4,9 +4,19 @@ namespace FbxSharp
 {
     public class TokenizationException : Exception
     {
-        public TokenizationException()
-            : base()
+        public TokenizationException(InputLocation location, string message)
+            : this(FormatMessage(location, message))
         {
+        }
+
+        public TokenizationException(InputLocation location, string message, Exception inner)
+            : this(FormatMessage(location, message), inner)
+        {
+        }
+
+	static string FormatMessage(InputLocation location, string message)
+	{
+            return string.Format("at {0}:{1},{2}: {3}", location.Filename, location.Line, location.Column, message);
         }
 
         public TokenizationException(string message)
