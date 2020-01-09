b204659e2d5832e353831500ffd41dd6e12b7ed1
izrik <izrik@izrik.com>
2020-01-09 10:58:52 -0600

    Add test targets for make.

--- all_build.sh
+++ all_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-(cd FbxCppTests && make all && ./bin/FbxCppTests ) && \
+(cd FbxCppTests && make all && make check ) && \
     nuget restore FbxSharpBuild.sln && \
     msbuild /p:Configuration=Debug FbxSharpBuild.sln && \
     mono ./packages/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe ./FbxSharpTests/bin/Debug/FbxSharpTests.dll
