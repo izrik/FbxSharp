5b2a922f8ed03b61d7dd40cc7d202901aa69d068
izrik <izrik@izrik.com>
2020-01-10 16:24:48 -0600

    Update build script.

--- all_build.sh
+++ all_build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
-(cd FbxCppTests && make all && make check ) && \
+(cd fbxcppcommon && make ) && \
+    (cd FbxCppTests && make all && make check ) && \
+    (cd fbxcppcli && make ) && \
     nuget restore FbxSharpBuild.sln && \
     msbuild /p:Configuration=Debug FbxSharpBuild.sln && \
     mono ./packages/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe ./FbxSharpTests/bin/Debug/FbxSharpTests.dll
