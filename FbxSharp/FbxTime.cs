23c81e521b402141aeed388feb67e732bffd720e
izrik <izrik@izrik.com>
2020-01-20 23:37:26 -0600

    Rename the const field to match the SDK. Add a couple more related fields.

--- FbxSharp/FbxTime.cs
+++ FbxSharp/FbxTime.cs
@@ -8,8 +8,12 @@ namespace FbxSharp
         public static readonly FbxTime Zero = new FbxTime(0);
 
         public const long UnitsPerSecond = 141120000L;
+
         public const long FBXSDK_TC_MILLISECOND = 141120L;
-        public const long FBXSDK_TC_MILLISECOND_LEGACY = 46186158L;
+        public const long FBXSDK_TC_LEGACY_MILLISECOND = 46186158L;
+
+        public const int FBXSDK_TC_STANDARD_DEFINITION = 0;
+        public const int FBXSDK_TC_LEGACY_DEFINITION = 127;
 
         public FbxTime(long time)
         {
