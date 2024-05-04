afaced49e8b0d8dfc0335020b3b4eadc2d76aed4
izrik <izrik@izrik.com>
2024-05-04 15:12:17 -0500

    Update build scripts.

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-nuget restore FbxSharpBuild.sln
-msbuild /p:Configuration=Debug FbxSharpBuild.sln
-mono ./packages/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe ./FbxSharpTests/bin/Debug/FbxSharpTests.dll
+dotnet build FbxSharp.sln
+dotnet test FbxSharpTests/
