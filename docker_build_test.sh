a0ac321a58465b72561970a20c8be82d3302bcde
izrik <izrik@izrik.com>
2024-05-09 12:01:05 -0500

    Add cli params for overriding the image and not running setup.

--- docker_build_test.sh
+++ docker_build_test.sh
@@ -2,9 +2,32 @@
 
 __DIR__="$(dirname $(realpath $BASH_SOURCE))"
 
+IMAGE=ubuntu:24.04
+SETUP=1
+while [[ -n "$1" ]] ; do
+  case "$1" in
+    "--image")
+      IMAGE="$2"
+      shift
+      ;;
+    "--setup")
+      SETUP=1
+      ;;
+    "--no-setup")
+      SETUP=""
+  esac
+  shift
+done
+
+if [[ -n "$SETUP" ]] ; then
+  COMMAND="/host/.github/workflows/build_setup.sh && /host/cpp_tests.sh"
+else
+  COMMAND="/host/cpp_tests.sh"
+fi
+
 docker run \
   --rm -it \
   -v "$PWD:/host" \
   -e "SUDO=pseudo" \
-  ubuntu:24.04 \
-  bash -c "/host/.github/workflows/build_setup.sh && /host/cpp_tests.sh"
+  "$IMAGE" \
+  bash -c "$COMMAND"
