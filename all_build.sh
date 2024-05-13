fec6a49352d292d01d3e13a0aa79183037350417
izrik <izrik@izrik.com>
2024-05-13 13:11:06 -0500

    Just do the one command.

--- all_build.sh
+++ all_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -xe
 set -e
-(cd fbxcppcommon && make ) && \
-    dotnet build FbxSharp.sln
+#(cd fbxcppcommon && make ) && \
+dotnet build FbxSharp.sln
 ls -laR TestCaseGenerator/bin/* || true
-(cd FbxCppTests && make all && make check ) && \
-    (cd fbxcppcli && make ) && \
-    dotnet test FbxSharpTests/
+#(cd FbxCppTests && make all && make check ) && \
+#    (cd fbxcppcli && make ) && \
+#    dotnet test FbxSharpTests/
