617632fe7e53100eba568777e9d93785e1b93c72
izrik <izrik@izrik.com>
2024-05-14 11:13:06 -0500

    Make the gen scripts PWD-invariant.

--- FbxCppTests/gen_tests.sh
+++ FbxCppTests/gen_tests.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+__DIR__="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+__ROOT_DIR__="$(dirname "$(realpath "$__DIR__")")"
+
 DEBUG=
 if [[ "$1" == "--debug" ]]; then
     DEBUG=1
 fi
 
-for f in ../test-cases/*.tc
+for f in "$__ROOT_DIR__"/test-cases/*.tc
 do
   g=`basename $f .tc`
   if [[ -n "$DEBUG" ]]; then
     echo "Generating $g in C++"
   fi
-  dotnet ../TestCaseGenerator/bin/Debug/net8.0/TestCaseGenerator.dll cpp $f $g.cpp
+  dotnet "$__ROOT_DIR__/TestCaseGenerator/bin/Debug/net8.0/TestCaseGenerator.dll" cpp $f "$__DIR__/$g.cpp"
 done
 
