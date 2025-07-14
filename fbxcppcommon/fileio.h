7471a7c6191eb2d7e52a2eaf96b880a939b76171
izrik <izrik@izrik.com>
2025-07-14 11:01:48 -0500

    Expand the 'convert' command to use any format or version. Remove the 'unconvert' command.

--- fbxcppcommon/fileio.h
+++ fbxcppcommon/fileio.h
@@ -5,7 +5,7 @@
 #include <fbxsdk.h>
 
 FbxScene* Load(const char* filename, FbxManager* manager=NULL);
-void Save(const char* filename, FbxScene* scene, bool ascii);
+void Save(const char* filename, FbxScene* scene, const char* format, const char* version);
 void LoadAndPrint(const char* filename);
 
 #endif // __FBXCPPTESTS_FILEIO_H
